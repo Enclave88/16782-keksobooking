93c310dbc3055def3c53e275cff282b5a099f2b0
“Enclave88git config --global user.email “sutangu@gmail.com <sutangu@gmail.com>
2017-02-03 00:39:27 +0600

    next iteration of travis fixes

--- js/form.js
+++ js/form.js
@@ -6,22 +6,22 @@ var closeDialog = document.querySelector('.dialog__close');
 
 // 1.1
 for (var i = 0, pins = pinList.length; i < pins; i++) {
-  pinList[i].addEventListener('click', function () {
+  pinList[i].addEventListener('click', function (e) {
     removeActiveClass();
-    this.classList.add('pin--active');
+    e.currentTarget.classList.add('pin--active');
     dialog.style.visibility = 'visible';
   });
 }
 
 // 2.3.собираем псевдомассив всех пинов и удаляем у них класс active
-function removeActiveClass () {
+function removeActiveClass() {
   for (i = 0, pins = pinList.length; i < pins; i++) {
     pinList[i].classList.remove('pin--active');
   }
 }
 
 // 2.2.закрываем диалог и вызываем функцию удаления класса active
-function hideDialog () {
+function hideDialog() {
   dialog.style.visibility = 'hidden';
   removeActiveClass();
 }
@@ -66,10 +66,10 @@ capacity.selectedIndex = 1;
 
 roomNumber.addEventListener('click', function () {
   var roomIndex = roomNumber.selectedIndex;
-  roomIndex === 0 ? capacity.selectedIndex = 1 : capacity.selectedIndex = 0;
+  (roomIndex === 0) ? capacity.selectedIndex = 1 : capacity.selectedIndex = 0;
 });
 
 capacity.addEventListener('click', function () {
   var capacityIndex = capacity.selectedIndex;
-  capacityIndex === 0 ? roomNumber.selectedIndex = 1 : roomNumber.selectedIndex = 0;
+  (capacityIndex === 0) ? roomNumber.selectedIndex = 1 : roomNumber.selectedIndex = 0;
 });
