3b03ff11bd6867f5fee0bae67e18c11081e1657b
“Enclave88git config --global user.email “sutangu@gmail.com <sutangu@gmail.com>
2017-02-07 00:01:17 +0600

    html attributes as tabindex, aria role and -pressed added plus event listener on .close-dialog

--- js/form.js
+++ js/form.js
@@ -9,6 +9,17 @@ var INVISIBLE_CLASS = 'invisible';
 var FLAT_MIN_PRICE = 1000;
 var HOVEL_MIN_PRICE = 0;
 var PALACE_MIN_PRICE = 10000;
+var ENTER_KEY_CODE = 13;
+var ESCAPE_KEY_CODE = 27;
+
+// ARIA
+var setCloseDialogAriaPressed = function () {
+  return closeDialog.setAttribute('aria-pressed', 'true');
+};
+
+var getCloseDialogAriaPressed = function () {
+  return closeDialog.setAttribute('aria-pressed', 'false');
+};
 
 // 1.1
 for (var i = 0, pins = pinList.length; i < pins; i++) {
@@ -21,17 +32,17 @@ for (var i = 0, pins = pinList.length; i < pins; i++) {
 }
 
 // 2.3.собираем псевдомассив всех пинов и удаляем у них класс active
-function removeActiveClass() {
+var removeActiveClass = function () {
   for (i = 0, pins = pinList.length; i < pins; i++) {
     pinList[i].classList.remove(PIN_ACTIVE_CLASS);
   }
-}
+};
 
 // 2.2.закрываем диалог и вызываем функцию удаления класса active
-function hideDialog() {
+var hideDialog = function () {
   dialog.classList.add(INVISIBLE_CLASS);
   removeActiveClass();
-}
+};
 
 // 2.1.событие закрытия диалога
 closeDialog.addEventListener('click', hideDialog);
@@ -77,6 +88,23 @@ capacity.addEventListener('click', function () {
     roomNumber.selectedIndex = 1;
   } else {
     roomNumber.selectedIndex = 0;
+  }
+});
+
+var hideHandling = function () {
+  hideDialog();
+  setCloseDialogAriaPressed();
+};
+
+closeDialog.addEventListener('keydown', function (event) {
+  if (event.keyCode && event.keyCode === ENTER_KEY_CODE) {
+    hideHandling();
+    document.addEventListener('keydown', function (evt) {
+      debugger;
+      if(evt.keyCode && evt.keyCode === ESCAPE_KEY_CODE) {
+        hideHandling();
+      }
+    })
   }
 });
 
